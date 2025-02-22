5bdef08411f90ddab6eac6cd30068c2bb7252df3
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-02-22 12:15:39 +0100

    added links to help

--- main.go
+++ main.go
@@ -1,36 +1,105 @@
 package winfiledetails
 
-import "time"
+import (
+	"fmt"
+	"time"
 
-// FileEA represents an extended attribute of a Windows file
-type FileEA struct {
-	Name  string
-	Value []byte
-	Flags uint8
+	"golang.org/x/sys/windows"
+)
+
+type FileDetails struct {
+	CreationTime   time.Time
+	LastAccessTime time.Time
+	LastWriteTime  time.Time
+	FileVersion    string
 }
 
-// FileInformation provides methods for retrieving Windows file information
-type FileInformation struct {
-	retryAttempts int
-	retryDelay    time.Duration
-}
-
-// NewFileInformation returns a new FileInformation instance with default settings
-func NewFileInformation() *FileInformation {
-	return &FileInformation{
-		retryAttempts: 3,
-		retryDelay:    500 * time.Millisecond,
+func getInfo(filePath string) (*FileDetails, error) {
+	// Convert path to UTF-16
+	utf16Path, err := windows.UTF16PtrFromString(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert path to UTF-16: %w", err)
 	}
+
+	// Open file with required access flags
+	handle, err := windows.CreateFile(
+		utf16Path,
+		windows.FILE_READ_EA,
+		windows.FILE_SHARE_READ,
+		nil,
+		windows.OPEN_EXISTING,
+		windows.FILE_FLAG_BACKUP_SEMANTICS,
+		0,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer windows.Close(handle)
+
+	// Get timestamps
+	creationTime, lastAccessTime, lastWriteTime, err := getTimestamps(handle)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file time: %w", err)
+	}
+
+	fileVersion, err := getFileVersion(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file version: %w", err)
+	}
+
+	var details FileDetails
+
+	details.CreationTime = creationTime
+	details.LastAccessTime = lastAccessTime
+	details.LastWriteTime = lastWriteTime
+	details.FileVersion = fileVersion
+
+	return &details, nil
 }
 
-// GetExtendedAttributes retrieves extended attributes for a file
-func (fi *FileInformation) GetExtendedAttributes(path string) ([]FileEA, error) {
-	// Implementation with retry logic and error handling
-	return nil, nil
+func getTimestamps(handle windows.Handle) (creationTime, lastAccessTime, lastWriteTime time.Time, err error) {
+	var ctime, atime, wtime windows.Filetime
+	err = windows.GetFileTime(handle, &ctime, &atime, &wtime)
+	if err != nil {
+		return time.Time{}, time.Time{}, time.Time{}, fmt.Errorf("failed to get file time: %w", err)
+	}
+
+	return time.Unix(0, ctime.Nanoseconds()), time.Unix(0, atime.Nanoseconds()), time.Unix(0, wtime.Nanoseconds()), nil
 }
 
-// SetExtendedAttributes sets extended attributes for a file
-func (fi *FileInformation) SetExtendedAttributes(path string, eas []FileEA) error {
-	// Implementation with retry logic and error handling
+func getFileVersion(filePath string) (string, error) {
+	var zHandle windows.Handle
+	// https://learn.microsoft.com/en-us/windows/win32/api/winver/nf-winver-getfileversioninfosizea
+	size, err := windows.GetFileVersionInfoSize(filePath, &zHandle)
+	if err != nil {
+		return "", fmt.Errorf("failed to get file version info size: %w", err)
+	}
+
+	// https://learn.microsoft.com/en-us/windows/win32/api/winver/nf-winver-getfileversioninfoa
+	err = windows.GetFileVersionInfo(filePath, 0, size, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to get file version info: %w", err)
+	}
+	fmt.Printf("Size: %v\n", size)
+	fmt.Printf("Handle: %v\n", zHandle)
+	return "", nil
+}
+
+func getFileInformation(handle windows.Handle) error {
+	var data windows.ByHandleFileInformation
+	err := windows.GetFileInformationByHandle(handle, &data)
+	if err != nil {
+		return fmt.Errorf("failed to get file information by handle: %w", err)
+	}
+
+	// Print file attributes
+	fmt.Printf("File Attributes: %v\n", data.FileAttributes)
+	fmt.Printf("Volume Serial Number: %v\n", data.VolumeSerialNumber)
+
+	fmt.Printf("In handle: %v\n", handle)
+
+	// windows.GetFileVersionInfo("C:\\Windows\\System32\\notepad.exe", uint32(handle), 0, nil)
+
 	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,18 +1,15 @@
 package winfiledetails
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"golang.org/x/sys/windows"
 )
 
 func TestNotExistingFile(t *testing.T) {
 	_, err := getInfo("C:\\Windows\\System32\\notepad2.exe")
 	require.ErrorContains(t, err, "The system cannot find the file specified.")
-
 }
 
 func TestFile(t *testing.T) {
@@ -22,57 +19,15 @@ func TestFile(t *testing.T) {
 	hundredYearsAgo := time.Now().AddDate(-100, 0, 0)
 	hundredYearsInFuture := time.Now().AddDate(100, 0, 0)
 
-	t.Logf("Creation Time: %s", d.creationTime.Format(time.RFC1123))
-	require.True(t, d.creationTime.After(hundredYearsAgo))
-	require.True(t, d.creationTime.Before(hundredYearsInFuture))
-	
-	t.Logf("Last Access Time: %s", d.lastAccessTime.Format(time.RFC1123))
-	require.True(t, d.lastAccessTime.After(hundredYearsAgo))
-	require.True(t, d.lastAccessTime.Before(hundredYearsInFuture))
+	t.Logf("Creation Time: %s", d.CreationTime.Format(time.RFC1123))
+	require.True(t, d.CreationTime.After(hundredYearsAgo))
+	require.True(t, d.CreationTime.Before(hundredYearsInFuture))
 
-	t.Logf("Last Write Time: %s", d.lastWriteTime.Format(time.RFC1123))
-	require.True(t, d.lastWriteTime.After(hundredYearsAgo))
-	require.True(t, d.lastWriteTime.Before(hundredYearsInFuture))
-}
+	t.Logf("Last Access Time: %s", d.LastAccessTime.Format(time.RFC1123))
+	require.True(t, d.LastAccessTime.After(hundredYearsAgo))
+	require.True(t, d.LastAccessTime.Before(hundredYearsInFuture))
 
-type FileDetails struct {
-	creationTime, lastAccessTime, lastWriteTime time.Time
-}
-
-func getInfo(filePath string) (*FileDetails, error) {
-	// Convert path to UTF-16
-	utf16Path, err := windows.UTF16PtrFromString(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert path to UTF-16: %w", err)
-	}
-
-	// Open file with required access flags
-	handle, err := windows.CreateFile(
-		utf16Path,
-		windows.FILE_READ_EA,
-		windows.FILE_SHARE_READ,
-		nil,
-		windows.OPEN_EXISTING,
-		windows.FILE_FLAG_BACKUP_SEMANTICS,
-		0,
-	)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to open file: %w", err)
-	}
-	defer windows.Close(handle)
-
-	// Get timestamps
-	var creationTime, lastAccessTime, lastWriteTime windows.Filetime
-	err = windows.GetFileTime(handle, &creationTime, &lastAccessTime, &lastWriteTime)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get file time: %w", err)
-	}
-	var details FileDetails
-
-	details.creationTime = time.Unix(0, creationTime.Nanoseconds())
-	details.lastAccessTime = time.Unix(0, lastAccessTime.Nanoseconds())
-	details.lastWriteTime = time.Unix(0, lastWriteTime.Nanoseconds())
-
-	return &details, nil
+	t.Logf("Last Write Time: %s", d.LastWriteTime.Format(time.RFC1123))
+	require.True(t, d.LastWriteTime.After(hundredYearsAgo))
+	require.True(t, d.LastWriteTime.Before(hundredYearsInFuture))
 }
