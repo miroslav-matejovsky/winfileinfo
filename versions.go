b1337aae1502e147d7eb58060d8e2ad7206a940e
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-06-28 08:31:28 +0200

    added certificates

---
+++ versions.go
@@ -0,0 +1,41 @@
+package winfileinfo
+
+import (
+	"fmt"
+
+	"golang.org/x/sys/windows"
+)
+
+type WinFileVersions struct {
+	FileVersion    WinFileVersion
+	ProductVersion WinFileVersion
+}
+
+type WinFileVersion struct {
+	Major uint16
+	Minor uint16
+	Patch uint16
+	Build uint16
+}
+
+func (f WinFileVersion) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", f.Major, f.Minor, f.Patch, f.Build)
+}
+
+// newWinFileInfo creates a new WinFileInfo from the given VS_FIXEDFILEINFO.
+func newWinFileInfo(vsFixedInfo *windows.VS_FIXEDFILEINFO) *WinFileVersions {
+	return &WinFileVersions{
+		FileVersion: WinFileVersion{
+			Major: uint16(vsFixedInfo.FileVersionMS >> 16),
+			Minor: uint16(vsFixedInfo.FileVersionMS & 0xffff),
+			Patch: uint16(vsFixedInfo.FileVersionLS >> 16),
+			Build: uint16(vsFixedInfo.FileVersionLS & 0xffff),
+		},
+		ProductVersion: WinFileVersion{
+			Major: uint16(vsFixedInfo.ProductVersionMS >> 16),
+			Minor: uint16(vsFixedInfo.ProductVersionMS & 0xffff),
+			Patch: uint16(vsFixedInfo.ProductVersionLS >> 16),
+			Build: uint16(vsFixedInfo.ProductVersionLS & 0xffff),
+		},
+	}
+}
--- winfiletime.go
+++ winfiletime.go
@@ -14,7 +14,7 @@ type WinFileTime struct {
 }
 
 // getFileTime retrieves the creation, last access, and last write times of the file.
-func (wf *WinFile) getFileTime() (*WinFileTime, error) {
+func (wf *WinFileInfo) getFileTime() (*WinFileTime, error) {
 	// Convert path to UTF-16
 	utf16Path, err := windows.UTF16PtrFromString(wf.path)
 	if err != nil {
